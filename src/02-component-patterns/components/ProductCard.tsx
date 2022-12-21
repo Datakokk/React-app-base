59f2c384d3c61b155d2f6d9ec8e78beae0c401ad
Datakokk <fiodorelzarster@gmx.com>
2022-12-21 05:05:41 +0100

    Starting Compound Component Pattern

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -10,18 +10,27 @@ interface Product {
     id: string,
     title: string,
     img?: string
-}
+};
 
-export const ProductCard = ({ product }: Props) => {
+export const ProductImage = ({img = ''}) =>  {
+    return (
+        <img className={ styles.productImg } src={ img ? img : noImage} alt="Product image" />
+    )
+};
 
-   const { counter, increaseBy } = useProduct();
+export const ProductTitle = ({ title }: { title: string }) => {
+    return (
+        <span className={ styles.productDescription }>{ title }</span>
+    )
+};
 
-  return (
-    <div className={ styles.productCard }>
-        <img className={ styles.productImg } src={ product.img ? product.img : noImage} alt={ product.title } />
-        {/* <img className={ styles.productImg } src={ noImage } alt="Coffe Mug" /> */}
+interface ProductButtonsProps {
+    counter: number,
+    increaseBy: (value: number) => void
+};
 
-        <span className={ styles.productDescription }>{ product.title }</span>
+export const ProductButtons = ({ counter, increaseBy }:ProductButtonsProps) => {
+    return (
         <div className={ styles.buttonsContainer }>
             <button
                 className={ styles.buttonMinus }
@@ -33,6 +42,24 @@ export const ProductCard = ({ product }: Props) => {
                 onClick={ () => increaseBy( +1 )}
                 > + </button>
         </div>
+    )
+};
+
+export const ProductCard = ({ product }: Props) => {
+
+   const { counter, increaseBy } = useProduct();
+
+  return (
+    <div className={ styles.productCard }>
+
+        <ProductImage img={ product.img }/>
+
+        <ProductTitle title={ product.title } />
+
+        <ProductButtons 
+            counter={ counter }
+            increaseBy={ increaseBy }
+        />
     </div>
   )
 }
